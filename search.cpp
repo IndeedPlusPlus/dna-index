bbe230d23a231249715707ceed2b32bebddfc9db
Indeed <indeed@indeed.moe>
2015-12-29 17:31:20 +0800

    create search implementation.

--- search.cpp
+++ search.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <iostream>
 #include <cassert>
+#include "common.hpp"
 
 using namespace std;
 
@@ -14,5 +15,30 @@ int main(int argc, char ** argv)
 		fprintf(stderr, "Usage: %s <input file> <index file>\n" , argv[0]);
 		return 1;
 	}
+	/* read subject string */
+	finput = fopen(argv[1], "r");
+	if (!finput) {
+		perror("[FATAL] open input file");
+		return 2;
+	}
+	char buf[255];
+	fgets(buf, 255 , fin);
+	sscanf(buf, "%u" , &n);
+	char * str = (char *) calloc( n + 1 , sizeof(char) );
+	fgets(str, n + 1, fin);
+	fclose(finput);
+	int * istr = transform_to_int(str, n);
+	assert(istr[n] == 1);
+	free(str);
+	str = NULL;
+
+	/* read bwt result */
+	findex = fopen(argv[2], "rb");
+	if (!findex) {
+		perror("[FATAL] open index file");
+		return 2;
+	}
+	int * sa =  (int *) calloc( n + 1 , sizeof(int) );
+	fread(sa, sizeof(int) , n + 1 , findex);
 	return 0;
 }
